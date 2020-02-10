c9e74dc92f6a462007530448f7c392933c508016
Zetwix <olehdobrii@gmail.com>
2020-02-10 16:39:50 +0000

    Comment

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const link = document.querySelector('a');
+
+link.textContent = 'Become true JS Jedi!';
+link.href = 'https://sj42.programmingmentor.com';
+
+const sect = document.querySelector('section');
+const para = document.createElement('p');
+sect.appendChild(para);
+para.textContent = 'We hope you enjoyed the ride.';
